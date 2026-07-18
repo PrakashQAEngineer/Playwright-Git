4832ce1867c387a76d66376c1d3c54e5c827e703
Prakash <im7prakash@gmail.com>
2026-07-18 11:25:54 +0530

    Fetch Text Value and Validate with assertion

--- tests/locator1.spec.js
+++ tests/locator1.spec.js
@@ -6,6 +6,12 @@ test('this is my first locator practice', async({page})=>
       await page.waitForTimeout(5000);
       await page.locator("input[id='username']").fill("Prakash SIngh");  //username
       await page.locator("#password").fill("123456");  //password
+      await page.locator("#terms").click();  //checkbox
+      await page.locator("#signInBtn").click();  //sign in button
+      const message = await page.locator("div[style*='block']").textContent();
+      console.log(message);
       await page.waitForTimeout(10000);
+      await expect(message).toBe('Incorrect username/password.');
+      
      }
 );
